7ccc258f68c0ca34b959b967aaa84561e27cbad8
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-07-30 01:10:31 +0800

    enable upcase and lowercase command

--- emacs/init.el
+++ emacs/init.el
@@ -5,6 +5,9 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
