564d3ad19a368f1e25f0daaeb94030618f700ab8
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-08-01 10:51:03 +0800

    增加书签配置

--- emacs/custom.el
+++ emacs/custom.el
@@ -17,7 +17,7 @@
 ;; 让 cnfonts 随着 Emacs 自动生效。
 (cnfonts-enable)
 ;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
-(cnfonts-set-spacemacs-fallback-fonts)
+;; (cnfonts-set-spacemacs-fallback-fonts)
   
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- emacs/init.el
+++ emacs/init.el
@@ -119,3 +119,8 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; open bookmark menu at startup
+;; (require 'bookmark)
+;; (bookmark-bmenu-list)
+;; (switch-to-buffer "*Bookmark List*")
